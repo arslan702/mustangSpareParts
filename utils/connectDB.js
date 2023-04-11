f119aa491d815ffa0646f41e831964d1969992e7
arslan456 <muhammadarslanjabbar@gmail.com>
2023-04-11 19:48:06 +0500

    'db-disconnected'

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
 
-const connectMongo = async () => mongoose.connect('mongodb+srv://kellyhood180:2OYYPY7HlTC4bHzs@cluster0.brikwpa.mongodb.net/?retryWrites=true&w=majority');
+const connectMongo = async () => mongoose.connect('mongodb+srv://(a)kellyhood180:2OYYPY7HlTC4bHzs@cluster0.brikwpa.mongodb.net/?retryWrites=true&w=majority');
 
 export default connectMongo
